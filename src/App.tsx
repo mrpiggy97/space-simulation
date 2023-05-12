6cac518cf02459c0b7edff2571a6a677ef053c0d
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-05-12 02:57:20 +0000

    added 3d models

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import './App.css'
+import { Canvas, useLoader } from '@react-three/fiber'
+import { TextureLoader } from 'three'
+import { OrbitControls } from '@react-three/drei'
+import texture from "./assets/solar-texture.jpg"
 
 function App() {
+  const map = useLoader(TextureLoader,texture)
 
   return (
     <div>
-      three app
+      <Canvas className='cv'>
+        <OrbitControls/>
+        <ambientLight intensity={1}/>
+        <directionalLight position={[6,2,3]} intensity={2}/>
+        <mesh rotation={[90,0,20]}>
+          <boxGeometry attach="geometry" />
+          <meshStandardMaterial attach="material" color="yellow" map={map} />
+        </mesh>
+      </Canvas>
     </div>
   )
 }
