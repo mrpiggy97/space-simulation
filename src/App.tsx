528de1765f392f24871fee8217b6764a89ded703
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-05-12 13:55:57 +0000

    deleted orbit controls, changed, changed position for first mesh

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { TextureLoader } from 'three'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Sphere, MeshDistortMaterial } from '@react-three/drei'
 import texture from "./assets/solar-texture.jpg"
 
 function App() {
@@ -10,13 +10,20 @@ function App() {
   return (
     <div>
       <Canvas className='cv'>
-        <OrbitControls/>
         <ambientLight intensity={1}/>
         <directionalLight position={[6,2,3]} intensity={2}/>
-        <mesh rotation={[90,0,20]}>
-          <boxGeometry attach="geometry" />
+        <mesh position={[5,0,0]} scale={1}>
+          <boxGeometry attach="geometry"/>
           <meshStandardMaterial attach="material" color="yellow" map={map} />
         </mesh>
+      </Canvas>
+      <Canvas>
+        <ambientLight intensity={1}/>
+        <OrbitControls enableZoom={false}/>
+        <directionalLight position={[6,2,3]} intensity={2}/>
+        <Sphere>
+          <MeshDistortMaterial distort={0.3} attach="material" color={"red"} map={map}/>
+        </Sphere>
       </Canvas>
     </div>
   )
