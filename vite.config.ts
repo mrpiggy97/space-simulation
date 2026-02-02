0609e321d21a7e636e31b1ac63623848d16a6925
mrpiggy97 <fabyjesusrivas10@gmail.com>
2026-02-02 00:13:17 +0000

    Updates vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    host: true,
+    port: 3000
+  },
+  preview:{
+    host: true,
+    port: 3000
+  },
+  build:{
+    rollupOptions:{
+      output: {
+        entryFileNames: 'main.js',
+        chunkFileNames: 'chunk-[hash].js',
+        assetFileNames: 'asset[extname]',
+      },
+    }
+  },
 })
